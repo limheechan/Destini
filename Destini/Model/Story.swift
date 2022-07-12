fcde65c4dca2067449c616d12ff46c68e6a44ffe
limheechan <decodeode@gmail.com>
2022-07-12 16:17:03 +0900

    MVC 패턴적용을 위한  초기 파일링 작업

---
+++ Destini/Model/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  Destini
+//
+//  Created by 임희찬 on 2022/07/12.
+//
+
+import Foundation
---
+++ Destini/Model/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  Destini
+//
+//  Created by 임희찬 on 2022/07/12.
+//
+
+import Foundation
