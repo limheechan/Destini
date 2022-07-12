168fb76597687f8ed38178f86b9ee218c15cf96b
limheechan <decodeode@gmail.com>
2022-07-12 17:38:09 +0900

    MVC 적용하며 전체 기능 적용 및 디자인 작업 약간

--- Destini/Controller/ViewController.swift
+++ Destini/Controller/ViewController.swift
@@ -9,11 +9,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    
+    @IBOutlet weak var choice1Button: UIButton!
+    
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBraing = StoryBrain()
+    
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateUI()
+     
     }
-
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userAnswer = sender.currentTitle
+        
+        if let userAnswer = userAnswer {
+            storyBraing.userSelcte(userAnswer: userAnswer)
+        }else { print("nil")}
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
+        
+        
+    }
+    
+    @objc func updateUI(){
+        choice1Button.alpha = 0.9
+        choice2Button.alpha = 0.9
+        storyLabel.layer.borderWidth = 3.0
+        storyLabel.layer.borderColor = UIColor.white.cgColor
+        
+        storyLabel.layer.cornerRadius = 0.5 * storyLabel.bounds.size.width
+        
+        
+        storyLabel.text = storyBraing.stories[storyBraing.storyNumber].title
+        choice1Button.setTitle(storyBraing.stories[storyBraing.storyNumber].choice1, for: .normal)
+        choice2Button.setTitle(storyBraing.stories[storyBraing.storyNumber].choice2, for: .normal)
+        
+    }
 
 }
 
